a0425c390da15ae802ffb7ec37441e9fa091e809
Gabe Finch <gabefinch@gmail.com>
2015-09-01 11:46:17 -0700

    Try binread

--- app.rb
+++ app.rb
@@ -1,16 +1,20 @@
 require 'net/http'
 require 'json'
+require 'base64'
 
 uri = URI('http://api.perka.com/1/communication/job/apply')
 req = Net::HTTP::Post.new(uri)
 req.content_type = 'application/json'
-req.body = {
+
+resume = File.binread("one_page_resume.pdf")
+coded_resume = Base64.encode64(resume)
+req.body = JSON.generate({
   first_name: "Gabe",
   last_name: "Finch",
   email: "gabefinch@gmail.com",
   position_id: "JS",
   explanation: "Ruby script, Base64 from web based converter.",
-  projects : [
+  projects: [
       "https://github.com/gabefinch/answer-me",
       "http://vast-shore-5799.herokuapp.com/",
       "https://github.com/gabefinch/flash-cards",
@@ -19,8 +23,8 @@ req.body = {
       "http://gabefinch.github.io/humble-pie-pizza/"
   ],
   source: "PDX_TechCrawl_2015",
-  resume:
-}
+  resume: coded_resume
+})
 
 res = Net::HTTP.start(uri.hostname, uri.port) do |http|
  http.request(req)
